564bc5f22225a800ba2977706bca111442b8c98f
doanthevu1910 <doanthevu1910@gmail.com>
2022-02-18 23:01:30 +0100

    Updated random-forest.py

--- correlation.py
+++ correlation.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sn
 
-df = pd.read_csv("data1.csv")
+df = pd.read_csv("data2.csv")
 
 data = df.drop(['price', 'postcode', 'year', 'latitude', 'longitude'], axis=1)
 
 corr_matrix = data.corr()
 
-sn.heatmap(corrMatrix, annot=True)
+sn.heatmap(corr_matrix, annot=True)
 plt.show()
