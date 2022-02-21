2327b309496e48f985cf1e7b23de50735fe96786
doanthevu1910 <doanthevu1910@gmail.com>
2022-02-21 11:11:47 +0100

    Updated gmaps.py

--- gmaps.py
+++ gmaps.py
@@ -1,17 +1,22 @@
 import gmaps
 import pandas as pd
+from ipywidgets.embed import embed_minimal_html
 
-df = pd.read_csv("data/data1.csv")
+df = pd.read_csv('data/data2.csv')
 
-gmaps.configure(api_key='api_key')
+df
+
+gmaps.configure(api_key='AIzaSyAoqfPlFnCtV5IeXSL_4s7ry0V1HQPGgD8')
 
 fig = gmaps.figure()
 heatmap_layer = gmaps.heatmap_layer(
-  df[['latitude','longitude']],
-  weights=df['house_price'],
-  max_intensity = 1000,
+  df[['latitude', 'longitude']],
+  weights=df['price'],
+  max_intensity=1000000,
   point_radius=6.0
 )
 
 fig.add_layer(heatmap_layer)
 fig
+
+embed_minimal_html('export.html', views=[fig])
